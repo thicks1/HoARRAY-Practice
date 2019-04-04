ec3103cdaeb45921af878bf0f9178db000c26b56
thicks1 <49279789+thicks1@users.noreply.github.com>
2019-04-04 09:50:06 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+	PART 1 
+ */
+
+// Task 1: Update the variable songList so that it is an array with at least 3 song titles.
+var songList=["You", "questions"];
+console.log(songList);
+
+// Task 2: Update the variable classList so that it is an array with 4 ScriptEd students (including yourself) in your class.
+var classList=["Tabytha","Tianna","Chenai","Chris"];
+console.log(classList);
+
+// Task 3: Update the variable teacherList so that it is an array with all the ScriptEd adults in your class.
+var teacherList= ["Joel", "Isaac","Riley", "Lian","Nickholaus","Attardi"];
+console.log(teacherList);
+
+// Task 4: Update the variable familyAges so that it is an array with a list of ages of your family members.
+var familyAges=[37,15,15,38];
+console.log(familyAges);
+
+
+
+/*
+	PART 2 
+ */
+
+// Task 5: Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
+var favSong;
+console.log(songList[0]);
+
+// Task 6: Update the variable bestStudent to retrieve your name from the classList array (Task 2).
+var bestStudent;
+console.log(classList[1]);
+
+// Task 7: Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
+var tallestTeacher;
+console.log(teacherList[0]);
+
+// Task 8: Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
+var oldestMember;
+console.log(familyAges[3]);
+
+
